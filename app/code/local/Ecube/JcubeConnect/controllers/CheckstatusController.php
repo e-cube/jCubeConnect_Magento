ca25e795260b3075d316f2bba7fe79a95710a756
Jeffrey Hullekes <jeffrey@e-cube.nl>
2014-03-14 18:38:50 +0100

    refactoring van alles

---
+++ app/code/local/Ecube/JcubeConnect/controllers/CheckstatusController.php
@@ -0,0 +1,14 @@
+<?php
+
+class Ecube_JcubeConnect_JcubeConnectController extends Mage_Core_Controller_Front_Action {
+
+    /**
+     * Controller index action
+     */
+    public function indexAction() {
+        // TODO: add checks to make sure Magento is alive
+        echo 'OK';
+    }
+
+}
+
